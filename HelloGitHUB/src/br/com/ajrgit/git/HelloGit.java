a47a38185d91f7daa0e76937c65bf1189fc67ada
AJr <53411942+alcidesjunior84@users.noreply.github.com>
2024-11-05 17:09:51 -0300

    Add upload

---
+++ HelloGitHUB/src/br/com/ajrgit/git/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.ajrgit.git;
+
+/**
+ * Exemplo de Integração
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
